195717b170287858c51b995e98f156794a73bff9
Joe Hingston <joe.hingston@worktribe.com>
2019-05-03 11:02:56 +0100

    add workflow schema

--- app/Rclass.php
+++ app/Rclass.php
@@ -4,21 +4,6 @@ namespace App;
 
 class Rclass extends Config
 {
-    public function contracts()
-    {
-        return $this->hasMany(Contract::class);
-    }
-
-    public function parties()
-    {
-        return $this->hasMany(Party::class);
-    }
-
-    public function notes()
-    {
-        return $this->hasMany(Note::class);
-    }
-
     public function actions()
     {
         return $this->hasMany(WorkflowAction::class);
